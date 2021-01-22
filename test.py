7044b2c76c9d7ebb6307661297739ba770f07266
n0hack <nohack-@naver.com>
2021-01-22 19:08:40 +0900

    update: defenition

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+# 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
